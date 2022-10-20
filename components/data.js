56138ce427112efc78a5cfa5846eca72fc9f8b01
Paul_Killian <psk65lava@gmail.com>
2022-10-19 21:04:46 -0700

    oer

---
+++ components/data.js
@@ -0,0 +1,28 @@
+export const speedway = {
+    company: 'SPEEDWAY MOTORS',
+    custNumber: '8238355',
+    upsNumber: '635358',
+    dropShip: 'DROPSHIP3',
+    notes: 'GROUND: PO# , SHIP UNINSURED, GROUND, PUT PO# IN UPS REFBOX#1. TRUCK FRIEGHT: PO# , SHIPS TRUCK FRIEGHT'    
+  }
+
+export const rpi = {
+    company: 'RPI DESIGNS',
+    custNumber: '8743326',
+    upsNumber: 'Y8Y794',
+    dropShip: 'DROPSHIP1'
+  }
+  
+export const windy = {
+    company: 'WINDY CITY MUSCLE CARS',
+    custNumber: '8449560',
+    upsNumber: 'None',
+    dropShip: 'UNDER $100 USE DROPSHIP2. OVER $100 USE DROPSHIP1'
+  }
+
+export const luttys = {
+    company: 'LUTTYS CHEVY',
+    custNumber: '324040',
+    upsNumber: '189743 only use if Requested by cust',
+    dropShip: 'None'
+  }
