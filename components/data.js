1191c6dae550319a1353b09a3446ba5c5b9a1f50
Paul_Killian <psk65lava@gmail.com>
2022-10-24 10:10:20 -0700

    PO#

--- components/data.js
+++ components/data.js
@@ -3,26 +3,30 @@ export const speedway = {
     custNumber: '8238355',
     upsNumber: '635358',
     dropShip: 'DROPSHIP3',
-    notes: 'GROUND: PO# , SHIP UNINSURED, GROUND, PUT PO# IN UPS REFBOX#1. TRUCK FRIEGHT: PO# , SHIPS TRUCK FRIEGHT'    
+    notes: 'GROUND: PO# , SHIP UNINSURED, GROUND, PUT PO# IN UPS REFBOX#1. TRUCK FRIEGHT: PO# , SHIPS TRUCK FRIEGHT',
+    po: 'PO#'
   }
 
 export const rpi = {
     company: 'RPI DESIGNS',
     custNumber: '8743326',
     upsNumber: 'Y8Y794',
-    dropShip: 'DROPSHIP1'
+    dropShip: 'DROPSHIP1',
+    po: 'PO#'
   }
   
 export const windy = {
     company: 'WINDY CITY MUSCLE CARS',
     custNumber: '8449560',
     upsNumber: 'None',
-    dropShip: 'UNDER $100 USE DROPSHIP2. OVER $100 USE DROPSHIP1'
+    dropShip: 'UNDER $100 USE DROPSHIP2. OVER $100 USE DROPSHIP1',
+    po: 'PO#'
   }
 
 export const luttys = {
     company: 'LUTTYS CHEVY',
     custNumber: '324040',
     upsNumber: '189743 only use if Requested by cust',
-    dropShip: 'None'
+    dropShip: 'None',
+    po: 'PO#'
   }
