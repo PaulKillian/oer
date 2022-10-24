c9b56c06e94cd903be1f517ff36e55616e55415b
Paul_Killian <psk65lava@gmail.com>
2022-10-24 16:54:16 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -45,9 +45,11 @@ export default function Home() {
     <div id={styles.pointer}
       onClick={() =>  copy(selectCompany.upsNumber)}>Ups#: {selectCompany.upsNumber}
     </div>
-    <div id={styles.pointer}
-      onClick={() =>  copy(selectCompany.dropShip)}>{selectCompany.dropShip}
-    </div>
+    {selectCompany.dropShip &&
+      <div id={styles.pointer}
+        onClick={() =>  copy(selectCompany.dropShip)}>{selectCompany.dropShip}
+      </div>
+    }
     <div id={styles.pointer}
       onClick={() =>  copy(selectCompany.po)}>{selectCompany.po}
     </div>
