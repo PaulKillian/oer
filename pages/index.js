ffbc0ed60387cf0df551857a799e709c9308b4c7
Paul_Killian <psk65lava@gmail.com>
2022-10-25 09:51:10 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styles from "../styles/Home.module.css";
 
-import { speedway, rpi, windy, luttys } from '../components/data.js'
+import { speedway, rpi, windy, luttys, ground } from '../components/data.js'
 
 export default function Home() {
    const [selectCompany, setSelectCompany] = React.useState('');
