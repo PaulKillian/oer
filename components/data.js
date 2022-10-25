0829681b608d474d9a4e30bde36d1082d9302361
Paul_Killian <psk65lava@gmail.com>
2022-10-25 15:57:16 -0700

    Update data.js

--- components/data.js
+++ components/data.js
@@ -1,41 +1,44 @@
-export const categories = [
-  {
+export const ground = {
     custNumber: '284546',
     dropShip: 'DROPSHIP1',
     upsNumber: '36Y163',
     po: 'PO#'
-  },
-  {
+  }
+
+export const luttys = {
     custNumber: '324040',
     upsNumber: '189743 only use if Requested by cust',
     po: 'PO#'
-  },
-  {
+  }
+
+export const rpi = {
     custNumber: '8743326',
     upsNumber: 'Y8Y794',
     dropShip: 'DROPSHIP1',
     po: 'PO#'
-  },
-  {
+  }
+
+export const speedway = {
     custNumber: '8238355',
     upsNumber: '635358',
     dropShip: 'DROPSHIP3',
     ground: 'PO# , SHIP UNINSURED, GROUND, PUT PO# IN UPS REFBOX#1', 
     truck: 'PO# , SHIPS TRUCK FRIEGHT',
     po: 'PO#'
-  },
-  {
+  }
+
+export const summit = {
     custNumber: '706183',
     dropShip: 'DROPSHIP3',
     upsNumber: '44958X',
     truck: 'PO# , Ship Schneider Truck Freight. No Liftgate needed',
     po: 'PO#'
-  },
-  {
+  }
+
+export const windy = {
     custNumber: '8449560',
     po: 'PO#',
     under: 'DROPSHIP2',
     over: 'DROPSHIP1'
   }
- ]
 
