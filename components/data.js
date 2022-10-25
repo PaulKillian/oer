ea11abb419c0041a494400a2a8a4f148462f3eae
Paul_Killian <psk65lava@gmail.com>
2022-10-25 09:36:04 -0700

    Update data.js

--- components/data.js
+++ components/data.js
@@ -1,9 +1,13 @@
-export const speedway = {
-    custNumber: '8238355',
-    upsNumber: '635358',
-    dropShip: 'DROPSHIP3',
-    ground: 'PO# , SHIP UNINSURED, GROUND, PUT PO# IN UPS REFBOX#1', 
-    truck: 'PO# , SHIPS TRUCK FRIEGHT',
+export const groundUPMotors = {
+    custNumber: '284546',
+    dropShip: 'DROPSHIP1',
+    upsNumber: '36Y163',
+    po: 'PO#'
+  }
+
+export const luttys = {
+    custNumber: '324040',
+    upsNumber: '189743 only use if Requested by cust',
     po: 'PO#'
   }
 
@@ -11,6 +15,15 @@ export const rpi = {
     custNumber: '8743326',
     upsNumber: 'Y8Y794',
     dropShip: 'DROPSHIP1',
+    po: 'PO#'
+  }
+
+export const speedway = {
+    custNumber: '8238355',
+    upsNumber: '635358',
+    dropShip: 'DROPSHIP3',
+    ground: 'PO# , SHIP UNINSURED, GROUND, PUT PO# IN UPS REFBOX#1', 
+    truck: 'PO# , SHIPS TRUCK FRIEGHT',
     po: 'PO#'
   }
   
@@ -21,8 +34,3 @@ export const windy = {
     over: 'DROPSHIP1'
   }
 
-export const luttys = {
-    custNumber: '324040',
-    upsNumber: '189743 only use if Requested by cust',
-    po: 'PO#'
-  }
