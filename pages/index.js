a4fe3bd35ed6712c0baa38f86278c2616465d640
Paul_Killian <psk65lava@gmail.com>
2022-10-26 15:29:36 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
    if(!toCopy) {
      navigator.clipboard.writeText(company.upsNumber)
      copyMore()
-   else {
+ } else {
      navigator.clipboard.writeText(toCopy)
     }
   };
