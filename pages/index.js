2a9076e741563bc99b9f588ea31620c9329f1a43
Paul_Killian <psk65lava@gmail.com>
2022-10-27 07:52:55 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,9 +34,8 @@ export default function Home() {
   
   const upsNumber = () => {
    if (company.upsNumber) {
-      console.log(company.upsNumber)
      return (
-     <div id={'1'} className={styles.pointer}
+     <div id={styles.pointer}
        onClick={() =>  copy(company.upsNumber)}>Ups#: {company.upsNumber}
      </div>
     )
@@ -45,23 +44,8 @@ export default function Home() {
    }
  }
  
- const copyMoreMore = () => {
-   navigator.clipboard.writeText(company.dropShip)
- }
- 
- const copyMore = () => {
-   navigator.clipboard.writeText(company.po)
-   copyMoreMore()
- }
-  
  const copy = (toCopy) => {
-   if(!toCopy) {
-     document.getElementById("1").click();
-     document.getElementById("2").click();
-     document.getElementById("3").click();
- } else {
-     navigator.clipboard.writeText(toCopy)
-    }
+   navigator.clipboard.writeText(toCopy)
   };
    
 
@@ -77,27 +61,26 @@ export default function Home() {
          <option>SUMMIT</option>
          <option>WINDY</option>
        </select>
-       <div id={styles.pointer} className={styles.pointer}
+       <div id={styles.pointer} 
          onClick={() =>  copy(company.custNumber)}>Cust#: {company.custNumber}
        </div>
-       <div onClick={() =>  copy()}>ups, po, drop</div>
        {upsNumber()}
-       <div id={'2'} className={styles.pointer}
+       <div id={styles.pointer} 
          onClick={() =>  copy(company.po)}>{company.po}
        </div>
-       <div id={'3'} className={styles.pointer}
+       <div id={styles.pointer} 
          onClick={() =>  copy(company.dropShip)}>{company.dropShip}
        </div>
-       <div className={styles.pointer}
+       <div id={styles.pointer} 
          onClick={() =>  copy(company.ground)}>{company.ground}
        </div>
-       <div className={styles.pointer}
+       <div id={styles.pointer} 
          onClick={() =>  copy(company.truck)}>{company.truck}
        </div>
-       <div className={styles.pointer}
+       <div id={styles.pointer} 
          onClick={() =>  copy(company.under)}>Under $100: {company.under}
        </div>
-       <div className={styles.pointer}
+       <div id={styles.pointer} 
          onClick={() =>  copy(company.over)}>Over $100: {company.over}
        </div>
     </>
