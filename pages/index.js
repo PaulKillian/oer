2a9f2203bfdbb1d1d1d0e6833fdaf50397b901f3
Paul_Killian <psk65lava@gmail.com>
2022-10-28 10:33:42 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ export default function Home() {
       for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
       }
-      return color;
     setColors(color)
   })
   
