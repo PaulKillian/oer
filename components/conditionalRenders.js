49fcb67d8376bd03d3cd32bec03b970e822cf3cd
Paul_Killian <psk65lava@gmail.com>
2022-10-29 16:30:47 -0700

    select styles

---
+++ components/conditionalRenders.js
@@ -0,0 +1,66 @@
+// import { 
+//     speedway, 
+//     rpi, 
+//     windy,
+//     luttys, 
+//     ground, 
+//     summit, 
+//     central,
+//     parts 
+//   } from '../components/data.js'
+
+// export const upsNumber = () => {
+//     if (company.upsNumber) {
+//       return (
+//       <div id={styles.pointer}
+//         onClick={() =>  copy(company.upsNumber)}>Ups#: {company.upsNumber}
+//       </div>
+//      )
+//     } else {
+//       return (
+//       <div id={styles.hidden}
+//         onClick={() =>  copy(company.upsNumber)}>Ups#: {company.upsNumber}
+//       </div>
+//      )
+//     }
+//   }
+  
+// export const under = () => {
+//     if (company.under) {
+//       return (
+//       <div id={styles.pointer}
+//         onClick={() =>  copy(company.under)}>Under $100: {company.under}
+//       </div>
+//      )
+//     } else {
+//       return (
+//       <div id={styles.hidden}
+//         onClick={() =>  copy(company.under)}>Under $100: {company.under}
+//       </div>
+//      )
+//     }
+//   }
+ 
+// export const over = () => {
+//    if (company.over) {
+//      return (
+//      <div id={styles.pointer}
+//        onClick={() =>  copy(company.over)}>Over $100: {company.over}
+//      </div>
+//     )
+//    } else {
+//      return (
+//      <div id={styles.hidden}
+//        onClick={() =>  copy(company.over)}>Over $100: {company.over}
+//      </div>
+//     )
+//    }
+//  }
+
+// export const conditionalDiv = (categories) => {
+//     return (
+//         <div id={styles}
+//           onClick={() =>  copy(categories)}>Ups#: {categories}
+//         </div>
+//        )
+//     }
