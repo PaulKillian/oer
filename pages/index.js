993668541b3bda0acfbdd56d6fd47dbcce2146c8
Paul_Killian <psk65lava@gmail.com>
2022-10-31 12:47:31 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,6 +74,11 @@ export default function Home() {
           {custNum(dealers)}
         </div>
         <div className='d-flex align-items-center'>
+          <div id='po' className={styles.pointer}
+            onClick={() =>  copy(event, dealers.po)}>{dealers.po}
+          </div>
+        </div>
+        <div className='d-flex align-items-center'>
           {truck(dealers)}
         </div>
         <div>
@@ -81,11 +86,6 @@ export default function Home() {
         </div>
         <div className='d-flex align-items-center'>
           {upsNum(dealers)}
-        </div>
-        <div className='d-flex align-items-center'>
-          <div id='po' className={styles.pointer}
-            onClick={() =>  copy(event, dealers.po)}>{dealers.po}
-          </div>
         </div>
         <div className='d-flex align-items-center'>
           <div id='dropShip' className={styles.pointer}
