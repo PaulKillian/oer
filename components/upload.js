78f0563e22193172bcdd7a86e5a729203281a389
Paul_Killian <psk65lava@gmail.com>
2022-11-01 14:51:26 -0700

    Update upload.js

--- components/upload.js
+++ components/upload.js
@@ -1,25 +1,36 @@
-// import {useState} from 'react';
-// import storage from './firebaseStorage';
+import {useState} from 'react';
 
-// export function Upload() {
-//     const allInputs = {imgUrl: ''}
-//     const [imageAsFile, setImageAsFile] = useState('')
-//     const [imageAsUrl, setImageAsUrl] = useState(allImputs)
+export function Upload() {
+  const [image, setImage] = useState({ preview: '', data: '' })
+  const [status, setStatus] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let formData = new FormData()
+    formData.append('file', image.data)
+    const response = await fetch('http://localhost:5000/image', {
+      method: 'POST',
+      body: formData,
+    })
+    if (response) setStatus(response.statusText)
+  }
 
-//     console.log(imageAsFile)
-//     const handleImageAsFile = (e) => {
-//       const image = e.target.files[0]
-//       setImageAsFile(imageFile => (image))
-//     }
+  const handleFileChange = (e) => {
+    const img = {
+      preview: URL.createObjectURL(e.target.files[0]),
+      data: e.target.files[0],
+    }
+    setImage(img)
+  }
 
-//     return (
-//       <div className="App">
-//         <form>
-//           <input 
-//             type="file"
-//             onChange={handleImageAsFile}
-//           />
-//         </form>
-//       </div>
-//     );
-//   }
+     return (
+       <div className="App">
+         <form onSubmit={handleSubmit}>
+           <input 
+             type="file"
+             onChange={handleImageAsFile}
+           />
+         <button type='submit'></button>
+         </form>
+       </div>
+     );
+   }
