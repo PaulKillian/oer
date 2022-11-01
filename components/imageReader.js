bd8a5b973e7c9d878f1720f4f33db5fdef3a71e4
Paul_Killian <psk65lava@gmail.com>
2022-11-01 12:40:04 -0700

    Update imageReader.js

--- components/imageReader.js
+++ components/imageReader.js
@@ -4,43 +4,6 @@ import $ from 'jquery';
 import Image from 'next/image'
 
 function ImageReader(packet){
-    useEffect(() => {
-        var log = document.getElementById('log');
- 
-    if(log.firstChild && log.firstChild.status === packet.status){
-        if('progress' in packet){
-            var progress = log.firstChild.querySelector('progress')
-            progress.value = packet.progress
-        }
-    } else {
-        var line = document.createElement('div');
-        line.status = packet.status;
-        var status = document.createElement('div')
-        status.className = 'status'
-        status.appendChild(document.createTextNode(packet.status))
-        line.appendChild(status)
- 
-        if('progress' in packet){
-            var progress = document.createElement('progress')
-            progress.value = packet.progress
-            progress.max = 1
-            line.appendChild(progress)
-        }
- 
- 
-        if(packet.status == 'done'){
-            log.innerHTML = ''
-            var pre = document.createElement('pre')
-            pre.appendChild(document.createTextNode(packet.data.text.replace(/\n\s*\n/g, '\n')))
-            line.innerHTML = ''
-            line.appendChild(pre)
-            $(".fas").removeClass('fa-spinner fa-spin')
-            $(".fas").addClass('fa-check')
-        }
- 
-        log.insertBefore(line, log.firstChild)
-    }
-    })
     
 
 // const { createWorker } = require("tesseract.js");
@@ -60,7 +23,6 @@ function ImageReader(packet){
         <select id="langsel">
          <option value='eng' selected> English </option>
         </select>
-        <input type="file" id="file-1" class="inputfile" />
         <Image id="selected-image"  alt='' src="" />
         <div id="log">
             <span id="startPre">  
