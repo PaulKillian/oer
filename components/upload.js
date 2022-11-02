29af09d33d13e277a5e55644dce10130e9eef498
Paul_Killian <psk65lava@gmail.com>
2022-11-02 10:18:42 -0700

    Update upload.js

--- components/upload.js
+++ components/upload.js
@@ -1,33 +1,21 @@
 import {useState} from 'react';
+import supabase from './supabase';
 
 export function Upload() {
-  const [image, setImage] = useState({ preview: '', data: '' })
-  const [status, setStatus] = useState('')
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    let formData = new FormData()
-    formData.append('file', image.data)
-    const response = await fetch('http://localhost:5000/image', {
-      method: 'POST',
-      body: formData,
+  const avatarFile = event.target.files[0]
+  const { data, error } = await supabase
+    .storage
+    .from('avatars')
+    .upload('public/avatar1.png', avatarFile, {
+      cacheControl: '3600',
+      upsert: false
     })
-    if (response) setStatus(response.statusText)
-  }
-
-  const handleFileChange = (e) => {
-    const img = {
-      preview: URL.createObjectURL(e.target.files[0]),
-      data: e.target.files[0],
-    }
-    setImage(img)
-  }
 
      return (
        <div className="App">
-         <form onSubmit={handleSubmit}>
+         <form>
            <input 
              type="file"
-             onChange={handleImageAsFile}
            />
          <button type='submit'></button>
          </form>
