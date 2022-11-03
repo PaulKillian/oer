ec976ca65d1eeb8d898652d9e967b1bfe2a36402
Paul_Killian <psk65lava@gmail.com>
2022-11-03 11:02:26 -0700

    Update upload.js

--- components/upload.js
+++ components/upload.js
@@ -35,14 +35,16 @@ export function Upload() {
         
     return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <input
-        type="file"
-        accept="image/*"
-        className="block w-auto text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-        id="file_input"
+       <input 
+        type="file" 
+        name="file" 
+        id="file" 
+        class="inputfile" 
         onChange={(event) => {
             handleUpload(event); // 👈 this will trigger when user selects the file.
         }}
+       />
+       <label for="file">Choose a file</label> 
       />
     <ImageReader url={url}/>
     </div>
