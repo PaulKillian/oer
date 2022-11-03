4af8fb2596dbf5588bf02a6e6a089ca797d397d3
Paul_Killian <psk65lava@gmail.com>
2022-11-03 08:08:21 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -63,7 +63,7 @@ export default function Home() {
             dealersAndImages={dealersAndImages}
           />
         </div>
-        <div className="d-flex align-items-center">
+        <div className="d-flex align-items-center justify-content-between">
           <div>
             <div style={{color: 'red'}} className={styles.pointer}>CHANGE ADDRESS!</div>
             <div className='d-flex align-items-center'>
