73515b808f542074fc0130d64250323c27c9fdde
Paul_Killian <psk65lava@gmail.com>
2022-11-03 10:04:09 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -62,6 +62,7 @@ export default function Home() {
             setimage={setimage}
             dealersAndImages={dealersAndImages}
           />
+        </div>
         <div className="constainer">
           <div className="row">
             <div className="col-sm">
