873a939d93c8865b3ce5b985aaf5981778712569
Paul_Killian <psk65lava@gmail.com>
2022-11-04 11:08:50 -0700

    Update imageReader.js

--- components/imageReader.js
+++ components/imageReader.js
@@ -40,9 +40,10 @@ function ImageReader(props) {
       
       values = text.substring(text.indexOf("Buyer:"));
       const buyer = values.substring(0, values.indexOf('SKU:'));
-      let splitBuyer = [];
-      splitBuyer = buyer.split(' ');
-      console.log(splitBuyer)
+      console.log(buyer)
+//       let splitBuyer = [];
+//       splitBuyer = buyer.split(' ');
+//       console.log(splitBuyer)
       
       values = text.substring(text.indexOf("SKU:"));
       const sku = values.substring(0, values.indexOf('Part Cost:'));
